de4c1539d5fa60964a1696f62ef4bfb71677ec5e
Oubayhe Ot <oubayhe.ot@gmail.com>
2025-08-26 23:06:17 +0100

    Created the Service for Gps Coordinates and the method to get all coordinates.

--- backend/src/main/java/com/backend/backend/services/GpsCoordinatesService.java
+++ backend/src/main/java/com/backend/backend/services/GpsCoordinatesService.java
@@ -1,10 +1,21 @@
 package com.backend.backend.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.backend.backend.models.GpsCoordinates;
+import com.backend.backend.repositories.GpsCoordinatesRepository;
 
 @Service
 public class GpsCoordinatesService {
-    public GpsCoordinatesService() {
+    private final GpsCoordinatesRepository gpsCoordinatesRepository;
+    
+    public GpsCoordinatesService(GpsCoordinatesRepository gpsCoordinatesRepository) {
+        this.gpsCoordinatesRepository = gpsCoordinatesRepository;
+    }
 
+    public List<GpsCoordinates> getAllCoordinates() {
+        return gpsCoordinatesRepository.findAll();
     }
 }
