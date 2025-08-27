bd88dc3969e1e277c38ce5ff87c48349183043df
Oubayhe Ot <oubayhe.ot@gmail.com>
2025-08-27 21:45:30 +0100

    Testing Leaflet, and displaying the Leaflet Map with a Marker.

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,12 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 })
 export class HomePage {
   constructor() {}
+
+  ngOnInit() {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
 }
